9d4824d13e25fc820c51a151ef0184ea618b2c70
thewizeguy89 <56734892+thewizeguy89@users.noreply.github.com>
2021-06-06 16:28:38 -0600

    create

---
+++ main.js
@@ -0,0 +1,40 @@
+const SHA256 = require('crypto-js/SHA256');
+
+class Block{
+	constructor(index, timestamp, data, previousHash = '') {
+		this.index = index;
+		this.timestamp = timestamp;
+		this.data = data;
+		this.previousHash = previousHash;
+		this.hash = this.calculateHash();
+	}
+
+	calculateHash(){
+		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).tostring();
+
+	}
+}
+
+class Blockchain{
+	constructor(){
+		this.chain = [this.createGenesisBlock()];
+	}
+	createGenesisBlock(){
+		return new Block(0, "06/06/2021", "Genesis block", "0");
+	}
+	getLatestBlock(){
+		return this.chain[this.chain.length - 1];
+	}
+	addBlock(newBlock){
+		newBlock.previousHash = this.getLatestBlock().hash;
+		newBlock.hash = newBlock.calculateHash();
+		this.chain.push(newBlock);
+	}
+}
+
+let newCoin = new Blockchain();
+newCoin.addBlock(new Block(1, "06/06/2021", {ammount 4}));
+newCoin.addBlock(new Block(2, "06/06/2021", {ammount 10}));
+
+console.log(JSON.stringify(newCoin, null, 4));
+
